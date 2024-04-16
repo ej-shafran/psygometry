a95c131ba5bebba3cd68cc464f395d7615130159
ej-shafran <ejshafran@hilma.tech>
2024-04-16 23:06:55 +0300

    feat(psygometry): go template rendering

--- cmd/psygometry/psygometry.go
+++ cmd/psygometry/psygometry.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 	e := echo.New()
+
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+
+	e.Renderer = t
+
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, world!")
+		return c.Render(http.StatusOK, "hello", "world")
 	})
 	e.Logger.Fatal(e.Start(":1714"))
 }
